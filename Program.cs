e8b7aca1159d246d9c989a93e7ada106b28d6220
Artur <89503357000@ya.ru>
2022-08-23 13:44:18 +0600

    main

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+
+int[] numbers = new int [5];
+int count = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(100, 999);
+    if ((numbers[i] % 2) == 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);
+Console.WriteLine("[{0}]", string.Join(", ", numbers));
+
